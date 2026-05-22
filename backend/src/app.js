d1c3e5fe47dee4d62170f1a525151744d4a1025f
gauravbadhan-ST <gauravbadhan@stealthtechnocrats.com>
2026-05-22 17:49:30 +0530

    feat: configure CORS options for enhanced security and functionality

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,18 @@ import taskRoutes from "./routes/task.routes.js";
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "https://task-flow-assignment.vercel.app",
+  ],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
